a257e595b82c02bf1e011a583569b187983e41fd
SheepMan <pepito.mussiloni@gmail.com>
2023-06-10 22:59:44 +0300

    implementing multiprocessing

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import multiprocessing
 import time
 import logging
 import sys
@@ -8,37 +9,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-logging.basicConfig(filename='script.log',filemode='w', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-sys.path.insert(0, 'W:\\my_project\\categories\\PC_and_Laptops')
-import PC_and_LaptopsCAT
-
-sys.path.insert(0, 'W:\\my_project\\categories\\PC_and_Laptops\\subcategories\\PC_components')
-import CPUs
-
-sys.path.insert(0, 'W:\\my_project\\categories\\PC_and_Laptops\\subcategories\\PC_components')
-import GPU
-
-sys.path.insert(0, 'W:\\my_project\\categories\\PC_and_Laptops\\subcategories\\PC_components')
-import Motherboards
-
-sys.path.insert(0, 'W:\\my_project\\categories\\PC_and_Laptops\\subcategories\\PC_components')
-import RAM
-
-sys.path.insert(0, 'W:\\my_project\\categories\\PC_and_Laptops\\subcategories\\PC_components')
-import pc_components_CAT
-
-sys.path.insert(0, 'W:\\my_project\\categories\\PC_and_Laptops\\subcategories\\PC_components')
-import Coollers
-
-sys.path.insert(0, 'W:\\my_project\\categories\\PC_and_Laptops\\subcategories\\PC_components')
-import PSUs
-
-sys.path.insert(0, 'W:\\my_project\\categories\\PC_and_Laptops\\subcategories\\PC_components')
-import SSDs
-
-sys.path.insert(0, 'W:\\my_project\\categories\\PC_and_Laptops\\subcategories\\PC_components')
-import Cases
 
 def run_Cases(browser):
     Cases.run(browser)
@@ -69,8 +40,7 @@ def run_pc_and_laptopsCAT(browser):
 
 def run_pc_componentsCAT(browser):
     pc_components_CAT.run(browser)
-
-
+    
 def run_script(browser):
     product_elements = browser.find_elements(By.CLASS_NAME, 'goods-tile__title')
     retry_attempts = 3
@@ -91,27 +61,95 @@ def run_script(browser):
     confirm_buy_button = browser.execute_script("return document.querySelector('a[href=\"https://rozetka.com.ua/ua/checkout/\"]')")
     browser.execute_script("arguments[0].click();", confirm_buy_button) # click using JavaScript
 
+def process_product_type(product_type):
+    browser = webdriver.Chrome()
+    run_pc_and_laptopsCAT(browser)  # starts category
+    time.sleep(1)
+    run_pc_componentsCAT(browser)  # starts subcategory
+    time.sleep(1)
+    if product_type == 'Cases':
+        run_Cases(browser)
+    elif product_type == 'SSDs':
+        run_SSDs(browser)
+    elif product_type == 'PSUs':
+        run_PSUs(browser)
+    elif product_type == 'Coollers':
+        run_Coollers(browser)
+    elif product_type == 'RAM':
+        run_RAM(browser)
+    elif product_type == 'CPUs':
+        run_CPUs(browser)
+    elif product_type == 'GPU':
+        run_GPU(browser)
+    elif product_type == 'Motherboards':
+        run_Motherboards(browser)
+    time.sleep(1)
+    run_script(browser)
+    time.sleep(10)
+    browser.quit()
+
+
+
 if __name__ == '__main__':
-    productType =[
-        ('Cases', run_Cases),
-        ('SSDs', run_SSDs),
-        ('PSUs', run_PSUs),
-        ('Coollers', run_Coollers),
-        ('RAM', run_RAM),
-        ('CPUs', run_CPUs),
-        ('GPU', run_GPU),
-        ('Motherboards', run_Motherboards)
-    ]
-    for productTypes_name, productType_script, in productType:
-        logging.info(f"Executing {productTypes_name} script")
-        browser = webdriver.Chrome()
-        run_pc_and_laptopsCAT(browser) # starts category
-        time.sleep(1)
-        run_pc_componentsCAT(browser) # starts subcategory
-        time.sleep(1)
-        productType_script(browser)
-        time.sleep(1)# delay
-        run_script(browser)
-        time.sleep(3)
-        browser.quit()
-        logging.info("All scripts executed")
+    logging.basicConfig(filename='script.log', filemode='w', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+    sys.path.insert(0, 'W:\\my_project\\categories\\PC_and_Laptops')
+    import PC_and_LaptopsCAT
+
+    sys.path.insert(0, 'W:\\my_project\\categories\\PC_and_Laptops\\subcategories\\PC_components')
+    import CPUs
+
+    sys.path.insert(0, 'W:\\my_project\\categories\\PC_and_Laptops\\subcategories\\PC_components')
+    import GPU
+
+    sys.path.insert(0, 'W:\\my_project\\categories\\PC_and_Laptops\\subcategories\\PC_components')
+    import Motherboards
+
+    sys.path.insert(0, 'W:\\my_project\\categories\\PC_and_Laptops\\subcategories\\PC_components')
+    import RAM
+
+    sys.path.insert(0, 'W:\\my_project\\categories\\PC_and_Laptops\\subcategories\\PC_components')
+    import pc_components_CAT
+
+    sys.path.insert(0, 'W:\\my_project\\categories\\PC_and_Laptops\\subcategories\\PC_components')
+    import Coollers
+
+    sys.path.insert(0, 'W:\\my_project\\categories\\PC_and_Laptops\\subcategories\\PC_components')
+    import PSUs
+
+    sys.path.insert(0, 'W:\\my_project\\categories\\PC_and_Laptops\\subcategories\\PC_components')
+    import SSDs
+
+    sys.path.insert(0, 'W:\\my_project\\categories\\PC_and_Laptops\\subcategories\\PC_components')
+    import Cases
+
+    pool = multiprocessing.Pool()
+    product_types = ['Cases', 'SSDs', 'PSUs', 'Coollers', 'RAM', 'CPUs', 'GPU', 'Motherboards']
+    pool.map(process_product_type, product_types)
+    pool.close()
+    pool.join()
+    result_values = multiprocessing.Array('i', len(product_types))
+    for i, product_type in enumerate(product_types):
+        result = result_values[i].value
+        logging.info(f"Executing {result} script")
+
+    logging.info("All scripts executed")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
